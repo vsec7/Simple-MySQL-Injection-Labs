4427147ee6f394c194189f482434a8ffad6c1144
Versailles <vsec48@gmail.com>
2019-06-19 12:35:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php
-
+//error_reporting(0);
 include('config.php');
 include('waf.php');
-
-// Coded by Versailles - viloid - cans21
-// Sec7or Team - Surabaya HackerLink
-// Simple SQL injection in GET Method ( Union Based )
 
 if(isset($_GET['id'])){
 
@@ -15,25 +11,23 @@ if(isset($_GET['id'])){
 
 	/*
 	$id = waf_addslashes($_GET['id']);
-	mysqli_query($con,"SET NAMES gbk");  // Untuk bypass Addslashes() query Mysql harus menggunakan charset big5 / gbk (multibyte character)
+	mysqli_query($con,"SET NAMES gbk");  // Untuk bypass Addslashes() dengan kondisi charset harus big5 / gbk (multibyte character)
 	*/
 
-	$query = "SELECT * FROM gath39 WHERE id = $id LIMIT 0,1"; // Integer based
-    //$query = "SELECT * FROM gath39 WHERE id = '$id' LIMIT 0,1"; // String based
+	$query = "SELECT * FROM article WHERE id = $id LIMIT 0,1"; // Integer based
+    //$query = "SELECT * FROM article WHERE id = '$id' LIMIT 0,1"; // String based
     $result = mysqli_query($con,$query) or mysqli_error($con);
     $row = mysqli_fetch_array($result);
 
     /* Routed Injection
 
-    $query2 = "SELECT image, quote FROM gath39 WHERE id = '$row[id]' ORDER BY id ASC";
+    $query2 = "SELECT image, quote FROM article WHERE id = '$row[id]' ORDER BY id ASC";
     $result2 = mysqli_query($con,$query2) or mysqli_error($con);
     $row2 = mysqli_fetch_array($result2);
 	*/
 
-	/* ----------------------------------------
-	//Mencegah SQLi dengan Mysqli Prepare Statement 
-
-    $stmt = $con->prepare("SELECT * FROM gath39 WHERE id = ? LIMIT 0,1");
+	/* Mencegah SQLi dengan Mysqli Prepare Statement 
+    $stmt = $con->prepare("SELECT * FROM article WHERE id = ? LIMIT 0,1");
     $stmt->bind_param('s', $id);
     $stmt->execute();
     $row = $stmt->get_result()->fetch_assoc();
