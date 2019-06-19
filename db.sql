5b36cbe45f5867a3dfbf8f8e6adae11f7d0781dd
Versailles <vsec48@gmail.com>
2019-06-19 12:32:57 +0700

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,10 +1,17 @@
-DROP DATABASE IF EXISTS shl;
-CREATE DATABASE shl;
-CREATE TABLE IF NOT EXISTS shl.gath39
-		(
-			id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-			title CHAR(32) NOT NULL,
-			image CHAR(32) NOT NULL,
-			quote CHAR(50) NOT NULL
-		);
-INSERT INTO shl.gath39 VALUES (1, 'Surabaya Hacker Link', 'image/shl.jpg', 'Jangan Biarkan Manusia Menatap Langit Penuh Harap, Biarkan Langit Gelap Selamanya');
+DROP DATABASE IF EXISTS gath39;
+CREATE DATABASE gath39;
+CREATE TABLE IF NOT EXISTS gath39.login 
+	(
+		id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+		user VARCHAR(20) NOT NULL,
+		pass VARCHAR(40) NOT NULL
+	);
+INSERT INTO gath39.login (id, user, pass)VALUES('1','admin','shl1337');
+CREATE TABLE IF NOT EXISTS gath39.article
+	(
+		id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+		title CHAR(32) NOT NULL,
+		image CHAR(32) NOT NULL,
+		quote CHAR(50) NOT NULL
+	);
+INSERT INTO gath39.article VALUES (1, 'Surabaya Hacker Link', 'image/shl.jpg', 'Jangan Biarkan Manusia Menatap Langit Penuh Harap, Biarkan Langit Gelap Selamanya');
